c1c355d1847ae384c3eb76e705765e45dd0336d1
dnn-coder <dvivasmera1.2@gmail.com>
2023-03-17 10:58:34 -0500

    creacion del modelo usuarios

--- app.js
+++ app.js
@@ -1,17 +1,71 @@
 const express = require('express');
+const { Sequelize, DataTypes } = require('sequelize');
 
-const usuarios = [
-    { id: 1, name: 'DANNY' },
-    { id: 2, name: 'CARLOS' },
-    { id: 3, name: 'PEDRO' },
-    { id: 4, name: 'PABLO' },
-];
-const estudiantes = [
-    { id: 1, name: 'ANDREA' },
-    { id: 2, name: 'PEDRO' },
-    { id: 3, name: 'DANIELA' },
-    { id: 4, name: 'FABIAN' },
-];
+//conectarme a mi base de datos
+const db = new Sequelize({
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '123456',
+    port: 5432,
+    database: 'recuerdodb',
+});
+
+db.authenticate()
+    .then(() => console.log('Base de datos autenticada'))
+    .catch(err => console.log(err));
+
+db.sync()
+    .then(() => console.log('Base de datos sincronizada'))
+    .catch(err => console.log(err));
+
+//Crear nuestro primer modelo (tabla)
+const Usuarios = db.define('usuarios', {
+    codigo: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    cedula: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    nombres: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    sexo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    cargo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    usuario: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    tipo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'ACTIVO',
+    },
+});
 
 // iniciar la funcion app
 
@@ -21,12 +75,17 @@ app.use(express.json());
 
 //definir mis endpoints
 
-app.get('/usuarios', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'lista de usuarios ok...',
-        usuarios,
-    });
+app.get('/usuarios', async(req, res) => {
+    try {
+        const usuarios = await Usuarios.findAll();
+        res.status(200).json({
+            status: 'success',
+            message: 'lista de usuarios ok...',
+            usuarios,
+        });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 app.post('/usuarios', (req, res) => {
@@ -48,6 +107,22 @@ app.post('/usuarios', (req, res) => {
 app.get('/estudiantes', (req, res) => {
     res.status(200).json({
         estudiantes,
+    });
+});
+app.post('/estudiantes', (req, res) => {
+    const { name } = req.body;
+
+    const newEstudent = {
+        id: Math.floor(Math.random() * 100),
+        name,
+    };
+
+    estudiantes.push(newEstudent);
+
+    res.status(201).json({
+        status: 'success',
+        message: 'Lista de estudiantes',
+        newEstudent,
     });
 });
 
