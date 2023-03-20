1ed7d6c97a0956985be5c53d13e127f6183096b6
dnn-coder <dvivasmera1.2@gmail.com>
2023-03-20 07:58:55 -0500

    creacion de las funciones de controlador de estudiantes 'buscar por id, actualizar y eliminar'

--- controllers/estudiantes.controller.js
+++ controllers/estudiantes.controller.js
@@ -61,4 +61,98 @@ const createStudent = async(req, res) => {
     });
 };
 
-module.exports = { getAllStudents, createStudent };
+const getStudentById = async(req, res) => {
+    const { idest } = req.params;
+    const estudiante = await Estudiantes.findOne({ where: { idest } });
+
+    if (!estudiante) {
+        return res.status(404).json({
+            status: 'error',
+            message: 'estudiante no encontrado',
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        estudiante,
+    });
+};
+
+const updateStudent = async(req, res) => {
+    const { idest } = req.params;
+    const {
+        codest,
+        cedest,
+        expedido,
+        pnomest,
+        snomest,
+        papeest,
+        sapeest,
+        sexoest,
+        direcest,
+        barrio,
+        municipio,
+        fnacest,
+        observacionest,
+        claveest,
+        usuarios_codigo,
+        acudientes_coddocumento,
+    } = req.body;
+
+    const estudiante = await Estudiantes.findOne({ where: { idest } });
+
+    if (!estudiante) {
+        return res.status(404).json({
+            status: 'error',
+            message: 'usuario no encontrado',
+        });
+    }
+
+    await estudiante.update({
+        codest,
+        cedest,
+        expedido,
+        pnomest,
+        snomest,
+        papeest,
+        sapeest,
+        sexoest,
+        direcest,
+        barrio,
+        municipio,
+        fnacest,
+        observacionest,
+        claveest,
+        usuarios_codigo,
+        acudientes_coddocumento,
+    });
+
+    res.status(204).json({
+        status: 'success',
+    });
+};
+
+const deleteStudent = async(req, res) => {
+    const { idest } = req.params;
+    const estudiante = await Estudiantes.findOne({ where: { idest } });
+
+    if (!estudiante) {
+        return res.status(404).json({
+            status: 'error',
+            message: 'usuario no encontrado',
+        });
+    }
+
+    await estudiante.update({ status: 'ELIMINADO' });
+
+    res.status(200).json({
+        status: 'success',
+    });
+};
+
+module.exports = {
+    getAllStudents,
+    createStudent,
+    getStudentById,
+    updateStudent,
+    deleteStudent,
+};
--- routes/estudiantes.routes.js
+++ routes/estudiantes.routes.js
@@ -4,13 +4,18 @@ const express = require('express');
 const {
     getAllStudents,
     createStudent,
+    getStudentById,
+    updateStudent,
+    deleteStudent,
 } = require('../controllers/estudiantes.controller');
 
 const estudiantesRouter = express.Router();
 //definir mis endpoints
 
 estudiantesRouter.get('/', getAllStudents);
-
 estudiantesRouter.post('/', createStudent);
+estudiantesRouter.get('/:idest', getStudentById);
+estudiantesRouter.patch('/:idest', updateStudent);
+estudiantesRouter.delete('/:idest', deleteStudent);
 
 module.exports = { estudiantesRouter };
